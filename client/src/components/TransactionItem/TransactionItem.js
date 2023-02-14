afd0fb4023eab328061007554e1bbf02754d8f4e
DrSergej <sergejdr.33@gmail.com>
2023-02-14 14:16:38 +0100

    add: icons-zuordnung-done

--- client/src/components/TransactionItem/TransactionItem.js
+++ client/src/components/TransactionItem/TransactionItem.js
@@ -1,9 +1,142 @@
 import "./TransactionItem.css";
 
 const TransactionItem = (props) => {
+	const expensesIconsPath = "./images/catagoryIcons/Expenses";
+	const incomeIconsPath = "./images/catagoryIcons/income";
+	function pickIcon() {
+		console.log("inside pickIcon");
+		if (props.category === "food&drink") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/food&drink.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "shopping") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/shopping.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "insurance") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/insurance.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "taxes") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/taxes.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "rent") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/rent.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "transportation") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/transportation.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "personal") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/personal.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "healthcare") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/healthcare.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "others-exp") {
+			return (
+				<img
+					className="logo icon-color-expense"
+					src={`${expensesIconsPath}/others-exp.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "salary") {
+			return (
+				<img
+					className="logo icon-color-income"
+					src={`${incomeIconsPath}/salary.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "passive") {
+			return (
+				<img
+					className="logo icon-color-income"
+					src={`${incomeIconsPath}/passive-income.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "pension") {
+			return (
+				<img
+					className="logo icon-color-income"
+					src={`${incomeIconsPath}/pension.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "gifts") {
+			return (
+				<img
+					className="logo icon-color-income"
+					src={`${incomeIconsPath}/gifts.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+		if (props.category === "others-inc") {
+			return (
+				<img
+					className="logo icon-color-income"
+					src={`${incomeIconsPath}/others-inc.svg`}
+					alt="Icon"
+				/>
+			);
+		}
+	}
+
 	return (
 		<div className="transaction-item display-flex__centered">
-			<img className="logo" src="./images/Finco2.svg" alt="IMG" />
+			{pickIcon()}
+
 			<div className="middle-part display-flex__between">
 				<h3>{props.category}</h3>
 				<p>
@@ -11,7 +144,17 @@ const TransactionItem = (props) => {
 				</p>
 			</div>
 			<div className="display-flex__centered">
-				<h2>$ {props.amount}</h2>
+				<h2
+					style={{
+						color:
+							props.typeTransaction === "income"
+								? "#3a94ff"
+								: "orange",
+					}}
+				>
+					{props.typeTransaction === "expense" ? "-" : "+"}$
+					{props.amount}
+				</h2>
 			</div>
 		</div>
 	);
