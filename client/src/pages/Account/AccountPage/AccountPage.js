fb3eedbf1a36e7808c99681dc698ba2bd5758c3c
DrSergej <sergejdr.33@gmail.com>
2023-02-16 13:20:39 +0100

    add: account-page, added avatar to home, minor styling

---
+++ client/src/pages/Account/AccountPage/AccountPage.js
@@ -0,0 +1,130 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { apiBaseUrl } from "../../../api";
+import Avatar from "../../../components/Avatar/Avatar";
+import NavBar from "../../../components/NavBar/NavBar";
+import "./AccountPage.css";
+
+const AccountPage = ({ token }) => {
+	const [profile, setProfile] = useState([]);
+	const [errorMessage, setErrorMessage] = useState([]);
+
+	useEffect(() => {
+		fetch(`${apiBaseUrl}/users/profile`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${token}`,
+			},
+		})
+			.then((res) => res.json())
+			.then(({ status, result, error }) => {
+				if (status === "ok") {
+					setProfile(result);
+				} else {
+					setErrorMessage(error.message);
+				}
+			});
+	}, [token]);
+
+	return (
+		<div className="account-page">
+			<div className="display-flex__between">
+				<div>
+					<p>Welcome back,</p>
+					<h2>
+						{`${profile.firstName} 
+						${profile.lastName}`}
+					</h2>
+				</div>
+				<Avatar token={token} />
+			</div>
+			<Link to="/">
+				<div className="outer-wrapper__small">
+					<div className="wrapper-left">
+						<img
+							className="icon-color-account"
+							src="/images/my-wallet-icon.svg"
+							alt="wallet-icon"
+						/>
+						<h2>My wallet</h2>
+					</div>
+					<img
+						className="icon-color-account back-button"
+						src="/images/arrow-forward-icon.svg"
+						alt="arrow-forward"
+					/>
+				</div>
+			</Link>
+			<div className="middle-section-wrapper">
+				<div className="outer-wrapper__small-2 corner-top__rounded">
+					<div className="wrapper-left">
+						<img
+							className="icon-color-account"
+							src="/images/notifications-icon.svg"
+							alt="wallet-icon"
+						/>
+						<h2>Notification</h2>
+					</div>
+					<img
+						className="icon-color-account back-button"
+						src="/images/arrow-forward-icon.svg"
+						alt="arrow-forward"
+					/>
+				</div>
+				<Link to="/edit-profile">
+					<div className="outer-wrapper__small-2 settings">
+						<div className="wrapper-left">
+							<img
+								className="icon-color-account"
+								src="/images/settings-icon.svg"
+								alt="wallet-icon"
+							/>
+							<h2>Settings</h2>
+						</div>
+						<img
+							className="icon-color-account back-button"
+							src="/images/arrow-forward-icon.svg"
+							alt="arrow-forward"
+						/>
+					</div>
+				</Link>
+				<div className="outer-wrapper__small-2 corner-bottom__rounded">
+					<div className="wrapper-left">
+						<img
+							className="icon-color-account"
+							src="/images/FAQ-icon.svg"
+							alt="wallet-icon"
+						/>
+						<h2>FAQ</h2>
+					</div>
+					<img
+						className="icon-color-account back-button"
+						src="/images/arrow-forward-icon.svg"
+						alt="arrow-forward"
+					/>
+				</div>
+			</div>
+			<Link to="/logout">
+				<div className="outer-wrapper__small">
+					<div className="wrapper-left">
+						<img
+							className="icon-color-account"
+							src="/images/logout-icon.svg"
+							alt="wallet-icon"
+						/>
+						<h2>Logout</h2>
+					</div>
+					<img
+						className="icon-color-account back-button"
+						src="/images/arrow-forward-icon.svg"
+						alt="arrow-forward"
+					/>
+				</div>
+			</Link>
+			<NavBar />
+		</div>
+	);
+};
+
+export default AccountPage;
