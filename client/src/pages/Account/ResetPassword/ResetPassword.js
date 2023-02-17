cd5821418e98f1b939f15a9584ff130d85c5b5e1
YubiFuu <jess.supercode@gmail.com>
2023-02-17 11:40:17 +0100

    add styling: forgot password, reset password

--- client/src/pages/Account/ResetPassword/ResetPassword.js
+++ client/src/pages/Account/ResetPassword/ResetPassword.js
@@ -47,24 +47,40 @@ const ResetPassword = () => {
     }
 
     return (
-        <div>
-            <h2>Enter new password</h2>
-            <input
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="repeat password"
-                value={repeatPassword}
-                onChange={(e) => setRepeatPassword(e.target.value)}
-            />
-            <Button buttonName={"Reset Password"} function={resetPassword} />
-            <p>
-                {`${errorMessage}`} <Link to="/login">Login</Link>
-            </p>
+        <div className="reset-password">
+            <header className="display-flex__between">
+                <img
+                    className="logo"
+                    src="/images/Finco2.svg"
+                    alt="finco-logo"
+                />
+                <div></div>
+            </header>
+            <main>
+                <h1>Enter new password</h1>
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="repeat password"
+                    value={repeatPassword}
+                    onChange={(e) => setRepeatPassword(e.target.value)}
+                />
+                <Button
+                    buttonName={"Reset Password"}
+                    function={resetPassword}
+                />
+            </main>
+            <footer>
+                <p>
+                    Back to <Link to="/login">Login</Link>
+                </p>
+                <p className="error-message">{`${errorMessage}`}</p>
+            </footer>
         </div>
     );
 };
