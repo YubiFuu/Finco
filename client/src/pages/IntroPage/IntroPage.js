028f4bf28f20a46635b2f0493cfcc364083a78ca
YubiFuu <jess.supercode@gmail.com>
2023-02-20 13:55:02 +0100

    resolved warnings

--- client/src/pages/IntroPage/IntroPage.js
+++ client/src/pages/IntroPage/IntroPage.js
@@ -3,22 +3,22 @@ import { useNavigate } from "react-router-dom";
 import "./IntroPage.css";
 
 const IntroPage = ({ token }) => {
-	const navigate = useNavigate();
-	console.log("intro-page-token:", token);
-	console.log("typeof:", typeof token);
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			navigate("/home");
-		}, [6000]);
-		return () => clearTimeout(timer);
-	}, []);
-	return (
-		<div className="intro-page display-flex__centered direction-column">
-			<img src="/images/Finco2.svg" />
-			<h1>Finco Bank</h1>
-			<h2>Ultra Premium Deluxe services!</h2>
-		</div>
-	);
+    const navigate = useNavigate();
+    console.log("intro-page-token:", token);
+    console.log("typeof:", typeof token);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/home");
+        }, [6000]);
+        return () => clearTimeout(timer);
+    }, [navigate]);
+    return (
+        <div className="intro-page display-flex__centered direction-column">
+            <img src="/images/Finco2.svg" alt="finco logo" />
+            <h1>Finco Bank</h1>
+            <h2>Ultra Premium Deluxe services!</h2>
+        </div>
+    );
 };
 
 export default IntroPage;
