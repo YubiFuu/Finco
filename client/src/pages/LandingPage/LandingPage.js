5ce7cb02a78e41303fa6c07554e105282445de67
YubiFuu <jess.supercode@gmail.com>
2023-02-21 11:30:48 +0100

    change: text of landing page to lower case

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -3,41 +3,41 @@ import Button from "../../components/Button/Button";
 import "./LandingPage.css";
 
 const LandingPage = () => {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	function navigateLogin() {
-		navigate("/register");
-	}
-	return (
-		<div className="landing-page display-flex__centered direction-column">
-			<div className="landing-wrapper">
-				<div className="div-1 display-flex__centered direction-column">
-					<img src="/images/landing-pic1.svg" alt="landing logo 1" />
-					<h1>
-						Track your Spend and Income
-						<br /> effortlessly
-					</h1>
-					<h2>Track your money flow at your fingertips</h2>
-				</div>
-				<div className="div-2 display-flex__centered direction-column">
-					<img src="/images/landing-pic2.svg" alt="langing logo 2" />
-					<h1>
-						Analyze your
-						<br /> spending
-					</h1>
-					<h2>Smart Banking for smart People</h2>
-				</div>
-			</div>
-			<div className="button-wrapper">
-				<Button buttonName={"Get started ➤"} function={navigateLogin} />
-			</div>
-			<footer>
-				<p>
-					Already have an account? <Link to="/login">Log in</Link>
-				</p>
-			</footer>
-		</div>
-	);
+    function navigateLogin() {
+        navigate("/register");
+    }
+    return (
+        <div className="landing-page display-flex__centered direction-column">
+            <div className="landing-wrapper">
+                <div className="div-1 display-flex__centered direction-column">
+                    <img src="/images/landing-pic1.svg" alt="landing logo 1" />
+                    <h1>
+                        Track your Spend and Income
+                        <br /> effortlessly
+                    </h1>
+                    <h2>Track your money flow at your fingertips</h2>
+                </div>
+                <div className="div-2 display-flex__centered direction-column">
+                    <img src="/images/landing-pic2.svg" alt="langing logo 2" />
+                    <h1>
+                        Analyze your
+                        <br /> spending
+                    </h1>
+                    <h2>Smart banking for smart people</h2>
+                </div>
+            </div>
+            <div className="button-wrapper">
+                <Button buttonName={"Get started ➤"} function={navigateLogin} />
+            </div>
+            <footer>
+                <p>
+                    Already have an account? <Link to="/login">Log in</Link>
+                </p>
+            </footer>
+        </div>
+    );
 };
 
 export default LandingPage;
